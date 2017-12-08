23676719a0bda3e0f77b9e11773cfa0e3d56a2ed
Mark Reynoso <mreynoso@spu.edu>
2017-12-07 22:07:52 -0800

    traversal started, not working

--- src/trie.py
+++ src/trie.py
@@ -87,4 +87,27 @@ class Trie(object):
         """Traverse the depth of the trie from string, else root."""
         if not isinstance(string, str):
             raise TypeError('You can only traverse with a word.')
-        
+        string = list(string.lower())
+        output = []
+        paths = []
+        trace = self.root
+        while True:
+            if len(string) > 0:
+                s = string.pop(0)
+                if s in trace.children:
+                    output.append(s)
+                    trace = trace.children[s]
+            else:
+                if len(trace.children) > 1:
+                    for key in trace.children:
+                        import pdb; pdb.set_trace()
+                        paths.append(trace.children[key].letter)
+                elif len(trace.children) == 1:
+                    output.append(list(trace.children.keys())[0])
+                    trace = trace.children[output[-1]]
+                elif len(trace.children) == 0:
+                    if len(paths) > 0:
+                        trace = paths.pop(0)
+                    else:
+                        break
+        yield output.pop(0)
