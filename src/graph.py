81dae55fb78baa1f91ae5a9762f4a2c3e44f3c39
Mark Reynoso <mreynoso@spu.edu>
2017-12-10 14:33:06 -0800

    updated graph

--- src/graph.py
+++ src/graph.py
@@ -16,37 +16,27 @@ class Graph(object):
         """Return a list of edges in graph."""
         edges = []
         for key in self._graph:
-            for i in self._graph[key]:
-                edges.append((key, i))
+            for child in self._graph[key]:
+                edges.append((key, child, self._graph[key][child]))
         return edges
 
     def add_node(self, val):
         """Add a node with value of val to graph."""
-        self._graph[val] = {}
+        self._graph.setdefault(val, {})
 
     def add_edge(self, val1, val2, weight=0):
         """Add a new edge to graph between val1 & val2 as well as add vals."""
-        if val1 in self._graph and val2 in self._graph:
-            if val2 not in self._graph[val1]:
-                self._graph[val1][val2] = weight
-        elif val1 in self._graph and val2 not in self._graph:
-            self._graph[val2] = {}
-            self._graph[val1][val2] = weight
-        elif val2 in self._graph and val1 not in self._graph:
-            self._graph[val1] = {}
-            self._graph[val1][val2] = weight
-        else:
-            self._graph[val1] = {val2: weight}
-            self._graph[val2] = {}
+        self.add_node(val1)
+        self.add_node(val2)
+        self._graph[val1][val2] = weight
 
     def del_node(self, val):
         """Delete node w/val from graph, raises exception if not exist."""
         if val in self._graph:
             del self._graph[val]
             for key in self._graph:
-                for i in self._graph[key]:
-                    if i == val:
-                        del self._graph[key][val]
+                if val in self._graph[key]:
+                    del self._graph[key][val]
         else:
             raise ValueError('There is no node of that value in the graph.')
 
@@ -58,38 +48,22 @@ class Graph(object):
             else:
                 raise ValueError('These edges do not exist.')
         except KeyError:
-            raise ValueError('These edges do not exist.')
+            raise ValueError('This edge does not exist.')
 
     def has_node(self, val):
         """Return true or false if node has value."""
-        if val in self._graph:
-            return True
-        else:
-            return False
+        return val in self._graph
 
     def neighbors(self, val):
         """Return list of nodes connected node(val)."""
         try:
-            self._graph[val]
-            neighbors = []
-            for key in self._graph[val]:
-                if key not in neighbors:
-                    neighbors.append(key)
-            for key in self._graph:
-                for child in self._graph[key]:
-                    if child == val and key not in neighbors:
-                        neighbors.append(key)
-            else:
-                return neighbors
+            return list(self._graph[val].keys())
         except KeyError:
-            raise ValueError('This node dosent exit')
+            raise ValueError('This node does not exist')
 
     def adjacent(self, val1, val2):
         """Return true if edge between vals, else false, & error if no val."""
-        if val1 in self._graph and val2 in self._graph:
-            if val2 in self._graph[val1]:
-                return True
-            else:
-                return False
-        else:
-            raise ValueError('These edges do not exist.')
+        try:
+            return val2 in self._graph[val1]
+        except KeyError:
+            raise ValueError('{} is not a node in this graph'.format(val1))
